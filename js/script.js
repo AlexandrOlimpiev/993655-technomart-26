854da876c0e2838b5dd4f69b83124f4804ece42b
Alexandr Olimpiev <alexandr.olimpiev@gmail.com>
2019-10-27 17:50:23 +0200

    Предварительный вариант 3

--- js/script.js
+++ js/script.js
@@ -49,16 +49,38 @@ window.addEventListener("keydown", function (evt) {
 //Модальное окно сообщения
 var contactLink = document.querySelector(".modal-contact-btn");
 var contactWindow = document.querySelector(".modal-contact");
-var closeСontactWindow = contactWindow.querySelector(".modal-close-btn"); 
+var closeСontactWindow = contactWindow.querySelector(".modal-close-btn")
+var nameField = contactWindow.querySelector("[name=name]");
+var emailField = contactWindow.querySelector("[name=email]");
+var messageField = contactWindow.querySelector("[name=message]");
+var form = contactWindow.querySelector("form");
+var isStorageSupport = true;
+var storage1 = "";
+var storage2 = "";
+  
+try {
+  storage1 = localStorage.getItem("name");
+  storage2 = localStorage.getItem("email");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 contactLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   contactWindow.classList.remove("hidden");
+  if (storage1&&storage2) {
+    nameField.value = storage1;
+    emailField.value = storage2;
+    messageField.focus();
+  } else {
+    nameField.focus();
+  }  
 });
 
 closeСontactWindow.addEventListener("click", function (evt) {
   evt.preventDefault();
   contactWindow.classList.add("hidden");
+  contactWindow.classList.remove("modal-error");
 });
 
 window.addEventListener("keydown", function (evt) {
@@ -66,6 +88,20 @@ window.addEventListener("keydown", function (evt) {
     evt.preventDefault();
     if (!contactWindow.classList.contains("hidden")) {
       contactWindow.classList.add("hidden");
+      contactWindow.classList.remove("modal-error");
     }
   }
 });
+
+form.addEventListener("submit", function (evt) {
+  if (!nameField.value || !emailField.value) {
+    evt.preventDefault();
+    contactWindow.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("name", nameField.value);
+      localStorage.setItem("email", emailField.value);
+    }
+  }
+});
+
